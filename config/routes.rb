57c481cfbd22331a8b5e69e41af1b098fd0bd4f7
Parnian <parnian@parnians-mbp.home>
2017-03-16 16:37:52 +0100

    Add room views

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resource :users, only: [:show]
-  resource :rooms
+  resources :users, only: [:show]
+  resources :rooms
 end
