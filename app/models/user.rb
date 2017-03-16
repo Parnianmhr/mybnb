4b0fb9753ff6032e86b305b3c2dce9bbb66a6125
Parnian <parnian@parnians-mbp.home>
2017-03-16 13:37:24 +0100

    one-to-meny controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :rooms, dependent: :destroy
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
