81689f3053327548e6ee67f36a239eea08519ea6
Parnian <parnian@Parnians-MacBook-Pro.local>
2017-03-18 16:27:02 +0100

    Add many-to-many association between room and theme

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,7 @@
 class Room < ApplicationRecord
   belongs_to :user
-  
+  has_and_belongs_to_many :themes
+
   validates :home_type, presence: true
   validates :room_type, presence: true
   validates :accommodate, presence: true
