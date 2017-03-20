d8eaa689c353a51ac735f1734dd37add1a987bc4
Parnian <parnian@192.168.2.37>
2017-03-20 15:04:26 +0100

    one_to_one association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
   has_many :rooms, dependent: :destroy
-  
+  has_one :profile
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def has_profile?
+    profile.present? && !profile.id.nil?
+  end
+
+  def full_name
+    profile.full_name
+  end
 end
