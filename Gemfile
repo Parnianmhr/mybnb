6e46d205693856da82f6dfdfe4f3a1617b12bcdb
Parnian <parnian@parnians-mbp.home>
2017-03-22 16:06:08 +0100

    Carrierwave added and g uploader

--- Gemfile
+++ Gemfile
@@ -1,10 +1,11 @@
 source 'https://rubygems.org'
 
+ruby '2.3.1'
+
 git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
 end
-
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.1'
@@ -61,3 +62,5 @@ gem 'bootstrap-sass', '~> 3.3.6'
 gem 'devise', '~> 4.2'
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'rails_12factor', group: :production
+gem 'carrierwave', '0.11.2'
--- spec/views/shared/_navbar.html.erb_spec.rb
+++ spec/views/shared/_navbar.html.erb_spec.rb
@@ -1,35 +1,37 @@
 require 'rails_helper'
 
 describe "shared/_navbar.html.erb" do
+  before { login_as user }
   # Write an extra test for the navbar that checks if the log in and sign up links get rendered when the user is not signed in.
-  # context "with log in and sign up" do
-  #   it "has in navbar" do
-  #     visit root_url
-  #     expect(page).to have_text("Log In")
+  context "with log in and sign up" do
+    let(:user) { create :user }
+    it "has in navbar" do
+      visit root_url
+      expect(page).to have_content('Log In')
+    end
+  end
+
+  # before { sign_in user }
+  #
+  # context "without profile" do
+  #   let(:user) { create :user }
+  #
+  #   it "renders email" do
+  #     render
+  #     expect(rendered).to have_content user.email
   #   end
   # end
-
-  before { sign_in user }
-
-  context "without profile" do
-    let(:user) { create :user }
-
-    it "renders email" do
-      render
-      expect(rendered).to have_content user.email
-    end
-  end
-
-  context "with profile" do
-    let(:profile) { build :profile }
-    let(:user) { create :user, profile: profile }
-
-    it "renders first and last name" do
-      render
-      expect(rendered).to have_content profile.first_name
-      expect(rendered).to have_content profile.last_name
-    end
-  end
+  #
+  # context "with profile" do
+  #   let(:profile) { build :profile }
+  #   let(:user) { create :user, profile: profile }
+  #
+  #   it "renders first and last name" do
+  #     render
+  #     expect(rendered).to have_content profile.first_name
+  #     expect(rendered).to have_content profile.last_name
+  #   end
+  # end
 end
 
 # context "Home Page" do
