af37437d1d8f0aa82268cb48985498e61ece4a73
Parnian <parnian@parnians-mbp.home>
2017-03-23 10:57:26 +0100

    Through Association: View

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,14 +1,16 @@
 class BookingsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
-    @booking = current_user.bookings.create(booking_params)
+    @booking = current_user.bookings.create(booking_params.merge(room_id: params[:room_id]))
+    @booking.set_total_price
+    @booking.save
     redirect_to @booking.room, notice: "Thank you for booking!"
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:starts_at, :ends_at, :price, :total, :room_id)
+    params.require(:booking).permit(:starts_at, :ends_at)
   end
 end
