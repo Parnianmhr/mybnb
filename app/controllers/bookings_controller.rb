2176596a54f679dbe52f941d09793cb4e057f8c8
Parnian <parnian@parnians-mbp.home>
2017-03-23 12:53:17 +0100

    Implement Range Overlapping Algorithm

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,10 +2,15 @@ class BookingsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @booking = current_user.bookings.create(booking_params.merge(room_id: params[:room_id]))
-    @booking.set_total_price
-    @booking.save
-    redirect_to @booking.room, notice: "Thank you for booking!"
+    @booking = current_user.bookings.create(booking_params)
+    @booking.room_id = params[:room_id]
+    if @booking.room_available?
+      @booking.set_total_price
+      @booking.save
+      redirect_to @booking.room, notice: "Thank you for booking!"
+    else
+      redirect_to @boking.room, notice: "Sorry! This listing is not available during the dates you requested."
+    end
   end
 
   private
